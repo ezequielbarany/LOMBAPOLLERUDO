760106850765d24b1c2e271d3cc7d70b256cb80c
Universidad Tecnol¢gica Nacional - Facultad Regional Buenos Aires <asd@hotmail.com>
2014-10-29 22:43:47 -0300

    Cambio namespace de todas las clases

--- FrbaHotel/FrbaCore/Clases/Cliente.cs
+++ FrbaHotel/FrbaCore/Clases/Cliente.cs
@@ -8,14 +8,20 @@ namespace FrbaCore
 {
     public partial class Cliente
     {
+        //Database conection
         private FrbaDataContextDataContext _dataContext;
 
+        //Add atributes
+        private List<Reserva> reservas { get; set; }
+
+        //Contructors
         public Cliente()
         {
             string conection = "Data Source=localhost\\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014";
             _dataContext = new FrbaDataContextDataContext(conection);
         }
 
+        //Methods
         public Cliente dameClientesSelect(){
             Cliente c1 = _dataContext.Cliente.FirstOrDefault();
             return c1;
--- FrbaHotel/FrbaCore/Clases/HabitacionCuadruple.cs
+++ FrbaHotel/FrbaCore/Clases/HabitacionCuadruple.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace FrbaCore.Clases
+namespace FrbaCore
 {
     class HabitacionCuadruple : Habitacion
     {
