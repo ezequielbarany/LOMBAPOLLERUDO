241652308251bd0b773af8abdf0acdda44c318cb
msarli <sarlimaximiliano@gmail.com>
2014-11-29 17:00:35 -0300

    Update Cliente.cs

--- FrbaHotel/FrbaCore/Clases/Cliente.cs
+++ FrbaHotel/FrbaCore/Clases/Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,35 +8,54 @@ namespace FrbaCore
 {
     public partial class Cliente
     {
-        //Add atributes
-        private List<Reserva> reservas { get; set; }
 
-        //Contructors
+        //Constructors
         public Cliente()
         {
         }
-        /*
-        public Cliente(decimal _numeroIdentificacion,int _tipoIdentificacion)
-        {
-            //string conection = ConfigurationManager.ConnectionStrings["FRBAHOTEL_DATABASE"].ConnectionString;
-            string conection = "Data Source=localhost\\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014";
-            _dataContext = new FrbaDataContextDataContext(conection);
-            //Seteo cliente
-            //SP busca cliente
-        }*/
 
+        public Cliente(decimal _numeroIdentificacion, int _tipoIdentificacion)
+        {
+            //Me traigo un cliente en particular
+            Cliente dataBaseCliente = DataContextSingleton.Connection.Cliente.Where(x => x.numeroIdentificacion == _numeroIdentificacion && x.tipoIdentificacion == _tipoIdentificacion).FirstOrDefault();
+            this.numeroIdentificacion = dataBaseCliente.numeroIdentificacion;
+            this.tipoIdentificacion = dataBaseCliente.tipoIdentificacion;
+            this.nombre = dataBaseCliente.nombre;
+            this.apellido = dataBaseCliente.apellido;
+            this.fechaNacimiento = dataBaseCliente.fechaNacimiento;
+            this.mail = dataBaseCliente.mail;
+            this.direccion = dataBaseCliente.direccion;
+            this.altura = dataBaseCliente.altura;
+            this.piso = dataBaseCliente.piso;
+            this.depto = dataBaseCliente.depto;
+            this.nacionalidad = dataBaseCliente.nacionalidad;
+            this.telefono = dataBaseCliente.telefono;
+            this.localidad = dataBaseCliente.localidad;
+            this.habilitado = dataBaseCliente.habilitado;
+        }
+
+        //Public methods
+        public List<Cliente> listarClientes()
+        {
+           return DataContextSingleton.Connection.Cliente.ToList();
+        }
+
+        public List<Cliente> listarClientes(string nombre, string apellido, string tipoIdentificacion, string numeroIdentificacion, string email)
+        {
+            //return DataContextSingleton.Connection.Cliente.Where(x => x.nombre.Equals(nombre)).ToList();
+            return DataContextSingleton.Connection.Cliente.Where(x => (nombre.Trim() == "" || x.nombre == nombre) && (apellido.Trim() == "" || x.apellido == apellido)).ToList();
+        }
+
+
+        /*
         //Methods
         public List<Cliente> dameClientesSelect(){
             List<Cliente> c1 = DataContextSingleton.Connection.Cliente.ToList();
             return c1;
 
-        }
+        }*/
 
-        public List<Cliente> buscarClientes(string nombre,string apellido,string tipoIdentificacion,string numeroIdentificacion,string email)
-        {
-            //Aca iria el sp
-            return DataContextSingleton.Connection.Cliente.Where(x => x.nombre.Equals(nombre)).ToList();
-        }
+
 
 
     }
